17c13bcb77575c1cd75491cf506e194363e3a855
Song Yang <songyang1218@gmail.com>
2021-03-02 19:06:56 -0500

    Setup script for JSB Chorales

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Prepares the data folder and data sets
+
+if [ ! -d data ]; then
+	mkdir data
+fi
+
+cd data
+
+# JSB Chorales data set
+if [ ! -f "JSB Chorales.zip" ]; then
+	curl http://www-etud.iro.umontreal.ca/~boulanni/JSB%20Chorales.zip
+fi
+
+if [ ! -d "JSB Chorales" ]; then
+    unzip "JSB Chorales.zip"
+fi
+
+# Maestro data set
+## TODO
