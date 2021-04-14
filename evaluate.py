69660138f5c008237b2fcd8a861a19d246d55a9f
Song Yang <songyang1218@gmail.com>
2021-04-13 22:37:30 -0400

    Kenlm python 2

--- evaluate.py
+++ evaluate.py
@@ -1,16 +1,19 @@
-"""
+u"""
 Run as python 2
 """
-from statistics import mean
+from __future__ import with_statement
+from __future__ import absolute_import
+from numpy import average
 import kenlm
 
 # Loop on the entire test set for an average perplexity
 import os
+from io import open
 
 
-def perplexity(path: str):
+def perplexity(path):
     model = kenlm.Model(path)
-    test_set_folder = "data/JSB Chorales/valid/text/"
+    test_set_folder = u"data/JSB Chorales/valid/text/"
 
     perplexities = []
 
@@ -21,19 +24,19 @@ def perplexity(path: str):
     return perplexities
 
 def show_ngram_perplexities():
-    print("number of test \t max \t min \t mean")
-    for model in os.listdir("model/kenlm/"):
-        if model.endswith("arpa"):
+    print u"number of test \t max \t min \t mean"
+    for model in os.listdir(u"model/kenlm/"):
+        if model.endswith(u"arpa"):
             continue
-        print(model)
-        perplexities = perplexity(os.path.join("model/kenlm/", model))
-        print(len(perplexities), max(perplexities), min(
-            perplexities), mean(perplexities), sep='\t')
+        print model
+        perplexities = perplexity(os.path.join(u"model/kenlm/", model))
+        print u'\t'.join([unicode(len(perplexities)), unicode(max(perplexities)), unicode(min(
+            perplexities)), unicode(average(perplexities))])
 
 # look at the perplexities
 def main():
     show_ngram_perplexities()
 
 
-if __name__ == "__main__":
+if __name__ == u"__main__":
     main()
